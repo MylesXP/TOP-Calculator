70c3d4e486d52a088c905800d30c6c29e0e90b8b
MylesXP <mylesparkinson12@gmail.com>
2022-12-24 11:14:27 -0800

    Gave operator buttons classes, added operator logic
    Gave buttons that represent operators specific classes to keep track of them and to specifically select them for JS use.
    Subsequently added basic logic for how said operators will interact with the UI e.g. clearing the display and removing classes or only allowing one instance of a character to show on display.

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
-// Declaring variables for doc elements
+// Declare variables for doc elements
 let calcButton = document.querySelectorAll('.calc-button');
 let calcDisplay = document.getElementById('calc_display');
-
-// calcButton.forEach(x => x.addEventListener('click', () => calcDisplay.textContent = x.textContent))
+// let calcDisplayContent = calcDisplay.textContent;
+// Iterate over array of buttons, adding event listener that determines what button is pressed
 calcButton.forEach(x => {x.addEventListener('click', () => {
-
     if (x.textContent.includes(Number(x.textContent))) {
         calcDisplay.textContent += x.textContent
     }
+    
+    else if (x.classList.contains('calc-equals')){
+        calcButton.forEach(x => x.classList.remove('opActive'))
+    } 
+
+    else if (x.classList.contains('calc-clear')) {
+        calcDisplay.textContent = ''
+        calcButton.forEach(x => x.classList.remove('opActive'))
+    }
+
+    else if (x.classList.contains('cal-op'))  {
+        calcDisplay.textContent = ''
+        x.classList.add('opActive')
+    }
+
+    else if (x.classList.contains('calc-percent')){
+        calcDisplay.textContent = ''
+    }
+
+    else if (x.classList.contains('calc-decimal')) {
+        if (calcDisplay.textContent.includes('.')) {
+            return
+        } else {
+            calcDisplay.textContent += x.textContent
+        }
+    }
+    
+    
+    // else if (!x.classList.contains('calc-equals')){
+    //     calcDisplay.textContent = ''
+    //     x.classList.add('opActive')
+    // }
+    
+    
+    // else if (x.classList.contains('calc-clear')) {
+    //     calcDisplay.textContent = ''
+    //     calcButton.forEach(x => x.classList.remove('opActive'))
+    // } 
+
+
+
+
+    //seperate else if statement specifically fro posneg
 
 })})
 
-
-
-
-// Declaring functions for calc operations
+// Declare functions for calc operations
 let add = function (x,y) {
     return x + y;
 };
