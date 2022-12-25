95cc14b0d5f51f6b3c643e3a22d1597f6fbb66d3
MylesXP <mylesparkinson12@gmail.com>
2022-12-25 09:14:32 -0800

    Reworked code, made pos neg button work
    Reworked code to use the previously declared functions instead of multiple if/else statements,
    assigned said functions in HTML using the onclick attribute.
    Added functionality to positive and negative value buttons by multiplying the text content of the display by -1 each time clicked.

--- app.js
+++ app.js
@@ -1,77 +1,85 @@
 // Declare variables for doc elements
 let calcButton = document.querySelectorAll('.calc-button');
 let calcDisplay = document.getElementById('calc_display');
-// let calcDisplayContent = calcDisplay.textContent;
-// Iterate over array of buttons, adding event listener that determines what button is pressed
-calcButton.forEach(x => {x.addEventListener('click', () => {
-    if (x.textContent.includes(Number(x.textContent))) {
-        calcDisplay.textContent += x.textContent
-    }
-    
-    else if (x.classList.contains('calc-equals')){
-        calcButton.forEach(x => x.classList.remove('opActive'))
-    } 
+// let calcDisplayCont = calcDisplay
+// calcDisplayCont = '0'
 
-    else if (x.classList.contains('calc-clear')) {
-        calcDisplay.textContent = ''
-        calcButton.forEach(x => x.classList.remove('opActive'))
-    }
 
-    else if (x.classList.contains('cal-op'))  {
-        calcDisplay.textContent = ''
-        x.classList.add('opActive')
-    }
-
-    else if (x.classList.contains('calc-percent')){
-        calcDisplay.textContent = ''
-    }
-
-    else if (x.classList.contains('calc-decimal')) {
-        if (calcDisplay.textContent.includes('.')) {
-            return
-        } else {
-            calcDisplay.textContent += x.textContent
+// Declare function for number button presses
+let numButtons = function(x){
+    if (x == 0) {
+        if (calcDisplay.textContent == '0') {
+            return;
         }
     }
-    
-    
-    // else if (!x.classList.contains('calc-equals')){
-    //     calcDisplay.textContent = ''
-    //     x.classList.add('opActive')
-    // }
-    
-    
-    // else if (x.classList.contains('calc-clear')) {
-    //     calcDisplay.textContent = ''
-    //     calcButton.forEach(x => x.classList.remove('opActive'))
-    // } 
 
-
-
-
-    //seperate else if statement specifically fro posneg
-
-})})
+    calcDisplay.textContent += x
+}
 
 // Declare functions for calc operations
 let add = function (x,y) {
-    return x + y;
+    x + y;
 };
 
 let sub = function (x,y) {
-    return x - y;
+    x - y;
 };
 
 let multi = function (x,y){
-    return x * y;
+    x * y;
 };
 
 let divide = function (x,y) {
-    return x / y;
+    x / y;
 };
 
 let operate = function (x, op, y) {
     if (op == '+') {
-        return 
     }
 }
+
+let posneg = function () {
+    calcDisplay.textContent = (Number(calcDisplay.textContent) * -1).toString()
+}
+
+let equals = function () {
+    calcButton.forEach(x => x.classList.remove('opActive'))
+}
+
+let percentage = function () {
+    calcDisplay.textContent = ''
+}
+
+
+// Not clearing display or removing classes
+let clear = function () {
+    calcDisplay.value = '';
+    calcButton.forEach(x => x.classList.remove('opActive'));
+}
+
+
+// Iterate over array of buttons, adding event listener that determines what button is pressed
+calcButton.forEach(x => {x.addEventListener('click', () => {
+    
+    if (x.classList.contains('cal-op'))  {
+        calcDisplay.textContent = ''
+        x.classList.add('opActive')
+    } else if (x.classList.contains('calc-clear')) {
+        calcDisplay.textContent = '';
+        calcButton.forEach(x => x.classList.remove('opActive'));
+    } else if (x.classList.contains('calc-decimal')) {
+        if (calcDisplay.textContent.includes('.')) {
+            return;
+        } 
+        calcDisplay.textContent += '.'  
+    } 
+    
+    
+    // else if (x.classList.contains('calc-posneg')) {
+    //     calcDisplay.textContent = (Number(calcDisplay.textContent) * -1).toString()
+    // }
+
+})})
+
+
+
