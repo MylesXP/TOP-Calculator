f2ec35ffbb646d8e61d2b13a81d6914237f4c49f
MylesXP <mylesparkinson12@gmail.com>
2022-12-25 17:22:52 -0800

    Added per click button focus and bug fixes
    Added logic for operator buttons that only adds the 'opActive' class to one of them at a time. Fixed a bug where the output from decimal button replaces the text in display if the only text in it is 0. Also, if the display content is an empty string, and the decimal button is clicked, it will add a zero (mainly aesthetics purposes as it doesn't affect mathematical operations if 0 is left out)

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Declare variables for doc elements
 let calcButton = document.querySelectorAll('.calc-button');
 let calcDisplay = document.getElementById('calc_display');
+let num1;
+let num2;
 // let calcDisplayCont = calcDisplay
-// calcDisplayCont = '0'
 
 
 // Declare function for number button presses
@@ -11,7 +12,22 @@ let numButtons = function(x){
         if (calcDisplay.textContent == '0') {
             return;
         }
-    }
+    } 
+    
+    // else if (x == '.' && calcDisplay.textContent == ''){
+    //     calcDisplay.textContent += "0"}  
+        
+        else if (x == '.') {
+        if (calcDisplay.textContent.includes('.')) {
+            return;
+        } else if (calcDisplay.textContent == '') {
+            calcDisplay.textContent += "0"
+        }
+    } else if (!x == 0 && calcDisplay.textContent == 0) {
+        if(!calcDisplay.textContent.includes('.')){
+            calcDisplay.textContent = calcDisplay.textContent.replace('0', '');
+        } 
+    } 
 
     calcDisplay.textContent += x
 }
@@ -33,6 +49,22 @@ let divide = function (x,y) {
     x / y;
 };
 
+let posneg = function () {
+    if (calcDisplay.textContent === '') {
+        return;
+    }
+    calcDisplay.textContent = (Number(calcDisplay.textContent) * -1).toString()
+}
+
+let percentage = function () {
+    calcDisplay.textContent = ''
+}
+
+// Logic for equal button
+let equals = function () {
+    calcButton.forEach(x => x.classList.remove('opActive'))
+}
+
 
 // Takes in an operator as an argument and matches it to one of the above functions, and calls the matched function
 let operate = function (x, op, y) {
@@ -40,17 +72,7 @@ let operate = function (x, op, y) {
     }
 }
 
-let posneg = function () {
-    calcDisplay.textContent = (Number(calcDisplay.textContent) * -1).toString()
-}
 
-let equals = function () {
-    calcButton.forEach(x => x.classList.remove('opActive'))
-}
-
-let percentage = function () {
-    calcDisplay.textContent = ''
-}
 
 let clearDisplay = function () {
     calcDisplay.textContent = '';
@@ -61,15 +83,18 @@ let clearDisplay = function () {
 // Iterate over array of buttons, adding event listener that determines what button is pressed
 calcButton.forEach(x => {x.addEventListener('click', () => {
     
-    if (x.classList.contains('cal-op'))  {
+     if (x.classList.contains('cal-op'))  {
         calcDisplay.textContent = ''
+        calcButton.forEach(x => x.classList.remove('opActive'))
         x.classList.add('opActive')
-    } else if (x.classList.contains('calc-decimal')) {
-        if (calcDisplay.textContent.includes('.')) {
-            return;
-        } 
-        calcDisplay.textContent += '.'  
-    } 
+    }
+    
+    // else if (x.classList.contains('calc-decimal')) {
+    //     if (calcDisplay.textContent.includes('.')) {
+    //         return;
+    //     }
+    //     calcDisplay.textContent += x.textContent
+    // }
 })})
 
 
