d93894d9f61c2458790a9b9ede25282e5d584ac2
MylesXP <mylesparkinson12@gmail.com>
2022-12-27 10:31:43 -0800

    Added logic for storing num values
    Added logic for storing num values for use in math operations. Added logic for adding op-active class to operator buttons when clicked and removing them once another is selected.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Declare variables for doc elements
 let calcButton = document.querySelectorAll('.calc-button');
 let calcDisplay = document.getElementById('calc_display');
+let calcOpButtons = document.querySelectorAll('.calc-op')
 let num1;
 let num2;
 
 calcDisplay.textContent = '0';
+
 
 // Functions for math operations
 let add = function (x,y) {
@@ -24,21 +26,35 @@ let divide = function (x,y) {
 };
 
 
+// Stores the first number
+calcOpButtons.forEach(x => x.addEventListener('click', () => {
+    if (!num1) {
+        num1 = calcDisplay.textContent;
+        calcDisplay.textContent = '';
+    } 
+}))
+
+
 // Operate function that takes 2 nums and an operator and calls one of the functions above
-let operate = function (x, op, y) {
+let operate = function (op) {
+    num2 = calcDisplay.textContent
     switch (op) {
         case '+':
-            return add(x,y);
+            add(Number(num1),Number(num1));
         case '-':
-            return sub(x,y);
+            sub(Number(num1),Number(num1));
         case '*':
-            return multi(x,y);
+            multi(Number(num1),Number(num1));
         case '/':
-            return divide(x,y);
+            divide(Number(num1),Number(num1));
         default:
             console.log('ERROR')
-    }
+            console.log(Array.from(calcButton).find(x => x.classList.contains('op-active')).innerText)
+            console.log(typeof Array.from(calcButton).find(x => x.classList.contains('op-active')).innerText)
+    };
+    calcButton.forEach(x => x.classList.remove('op-active'));
 };
+
 
 // Functions that populate display with number button values
 let numButtons = function(x){
@@ -63,16 +79,27 @@ let numButtons = function(x){
 
     calcDisplay.classList.remove('calc-display__result')
     calcDisplay.textContent += x
-}
+};
+
 
 // Function for clearing the screen when clear button is clicked
 let clearDisplay = function () {
     num1 = '';
     num2 = '';
     calcDisplay.textContent = 0;
-    calcButton.forEach(x => x.classList.remove('opActive'));
+    calcButton.forEach(x => x.classList.remove('op-active'));
     calcDisplay.classList.remove('calc-display__result')
-}
+};
+
+
+// Logic for adding or removing active operator (op-active) class to operator buttons when clicked
+calcOpButtons.forEach(x => x.addEventListener('click', () => {
+    if (x.classList.contains('calc-op'))  {
+        // calcDisplay.textContent = ''
+        calcButton.forEach(x => x.classList.remove('op-active'))
+        x.classList.add('op-active')
+    }
+}))
 
 
 
